e9f684626d59066c6fd883514dcbf4126d59c8fe
Pabi691 <pabitra691@gmail.com>
2026-02-04 15:27:36 +0530

    Deploy

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "indianhairworld.com",
+          },
+        ],
+        destination: "https://www.indianhairworld.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     domains: ['indianhairworld.com', 'server.indianhairworld.com', 'api.indianhairworld.com'],
     remotePatterns: [
